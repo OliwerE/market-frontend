0f2d004d1d7912808ef3b50906e9ab0a396e01cb
Oliwer <oe222ez@student.lnu.se>
2021-04-08 16:44:46 +0200

    Created react router pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import Navbar from './components/Navbar'
@@ -7,17 +10,31 @@ import Start from './components/Start'
 import Buy from './components/Buy'
 import Sell from './components/Sell'
 import Help from './components/Help'
+import Error from './components/Error'
 
 function App() {
   return (
-    <div className="container">
-      <Navbar />
-      <Start />
-      <Buy />
-      <Sell />
-      <Help />
-      <Footer />
-    </div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Start />
+          </Route>
+          <Route exact path="/kop">
+            <Buy />
+          </Route>
+          <Route exact path="/salj">
+            <Sell />
+          </Route>
+          <Route exact path="/hjalp">
+            <Help />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
   );
 }
 
