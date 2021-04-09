4a1845db43f2e7606ae894f22fb962e20b4d0886
Oliwer <oe222ez@student.lnu.se>
2021-04-09 18:33:10 +0200

    Added login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
 const Login = ({ close }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(username, password)
+
+    // koppla ihop med backend.
+  }
+
   return (
       <div id="loginBox">
-        <button onClick={close}>Close!</button>
-        <h2>Login</h2>
+        <button onClick={close}>Stäng</button>
+        <h2>Logga in</h2>
+        <form onSubmit={handleSubmit}>
+          <label for="username">Användarnamn:</label><br>
+          </br><input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)}></input><br>
+          </br><label for="password">Lösenord:</label><br>
+          </br><input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}></input><br>
+          </br><button type="submit">Logga in</button>
+        </form>
+        
+        <p>Har du inget konto?</p>
+        <button>Skapa Konto</button>
       </div>
   )
 }
